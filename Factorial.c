ffa9ef2f3df8756ef7cc9ce1b7638755250f7dbe
Maindu26 <113236589+Maindu26@users.noreply.github.com>
2022-09-10 15:00:51 +0530

    Create Factorial.c

---
+++ Factorial.c
@@ -0,0 +1,15 @@
+#include<studio.h>
+#include< conio.h>
+Main()
+{
+  int i,n,fact=1;
+  Clrscr();
+  Scanf("%d",&i,&n);
+  While (i<=n)
+  {
+    Fact=fact×i;
+     I++;
+  }
+  Printf ("the fact value of given number");
+  Getch();
+}
